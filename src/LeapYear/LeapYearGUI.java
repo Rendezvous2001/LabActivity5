7fd02e2d24d8baba6a68b9d75722e4907c70ecce
Brian Despi <briandespi315@gmail.com>
2022-12-16 22:45:01 +0800

    "Complete LeapYear"

--- src/LeapYear/LeapYearGUI.java
+++ src/LeapYear/LeapYearGUI.java
@@ -10,6 +10,23 @@ public class LeapYearGUI extends JFrame{
     private JLabel inputYourLabel;
     private JButton btnCheckYear;
 
+    public LeapYearGUI() {
+        btnCheckYear.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                int year = Integer.parseInt(tfYear.getText());//for input
+
+                if (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0)) {
+                    JOptionPane.showMessageDialog(panel1, "Leap year");
+                } else {
+                    JOptionPane.showMessageDialog(panel1, "Not a leap year");
+                }
+
+            }
+        });
+
+    }
+
     public static void main(String[] args) {
 
         LeapYearGUI app = new LeapYearGUI();
